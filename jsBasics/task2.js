2e7cb3a0437d1fa28ba4c4c2d030ee633a349f51
Olga <olga.letinskaya@tateeda.com>
2026-01-14 11:46:39 +0200

    Add JS basics homework

---
+++ jsBasics/task2.js
@@ -0,0 +1,10 @@
+let name1 = "Anna";
+let name2 = "Mark";
+
+// Конкатенация
+let greeting1 = "Hello " + name1 + " and " + name2 + "!";
+console.log(greeting1);
+
+// Шаблонная строка
+let greeting2 = `Hello ${name1} and ${name2}!`;
+console.log(greeting2);
